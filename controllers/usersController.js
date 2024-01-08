e314e15f7f8588c48acfa39927a298a214344da0
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2024-01-08 02:03:15 -0600

    added to controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -77,4 +77,46 @@ const usersControllers = {
       res.statsu(400).json(err);
     }
   },
+
+  //To add friend to users friend list
+  async addFriend({ params }, res) {
+    try {
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: params.userId },
+        { $addToSet: { friends: params.friendId } },
+        { new: true }
+      );
+
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found" });
+        return;
+      }
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+  },
+
+  //To remove a friend from users list
+  async removeFriend({ params }, res) {
+    try {
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { friends: params.friendId } },
+        { new: true }
+      );
+
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found" });
+        return;
+      }
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+  },
 };
